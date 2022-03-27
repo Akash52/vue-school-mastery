d447990061ea46ec9f37e523975112996f10d833
Akash52 <ac8572611@gmail.com>
2022-03-27 22:44:54 +0530

    Create ThreadList.vue and Clean up the Homepage

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,74 +1,18 @@
 <template>
-  <div v-for="thread in threads" :key="thread.id" class="col-large push-top">
-    <h1>{{ thread.title }}</h1>
-
-    <div class="post-list">
-      <div class="post" v-for="postId in thread.posts" :key="postId">
-        <div class="user-info">
-          <a href="#" class="user-name">{{
-            userById(postById(postId).userId).name
-          }}</a>
-
-          <a href="#">
-            <img
-              class="avatar-large"
-              :src="userById(postById(postId).userId).avatar"
-              alt=""
-            />
-          </a>
-
-          <p class="desktop-only text-small">107 posts</p>
-        </div>
-
-        <div class="post-content">
-          <div>
-            <p>
-              {{ postById(postId).text }}
-            </p>
-          </div>
-        </div>
-
-        <div class="post-date text-faded">
-          {{ postById(postId).publishedAt }}
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <!-- <div v-for="thread in threads" :key="thread.id">
-    <div>
-      <h3>{{ thread.title }}</h3>
-      <div v-for="postId in thread.posts" :key="postId">
-        <p>
-          {{ getUser(getPost(postId).userId).name }}
-        </p>
-        <p>
-          {{ getPost(postId).text }}
-        </p>
-      </div>
-    </div>
-  </div> -->
+  <h1>Welcome to the Vue 3 Forum</h1>
+  <thread-list :threads="threads" />
 </template>
 
 <script>
 import sourceData from '@/data.json';
+import ThreadList from './ThreadList.vue';
+
 export default {
+  components: { ThreadList },
   data() {
     return {
       threads: sourceData.threads,
-      posts: sourceData.posts,
-      users: sourceData.users,
     };
-  },
-  methods: {
-    postById(postId) {
-      return this.posts.find((post) => post.id === postId);
-    },
-    userById(userId) {
-      return this.users.find((user) => user.id === userId);
-    },
   },
 };
 </script>
-
-<style></style>
