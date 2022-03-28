5996ed3b1e3f48a00258cf79125192c76752fb0c
Akash52 <ac8572611@gmail.com>
2022-03-28 10:58:34 +0530

    extract posts to a component

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -4,43 +4,20 @@
       <router-link to="/"> Home </router-link>
     </button>
     <h1>{{ thread.title }}</h1>
-    <div class="post-list">
-      <div class="post" v-for="postId in thread.posts" :key="postId">
-        <div class="user-info">
-          <a href="#" class="user-name">{{
-            userById(postById(postId).userId).name
-          }}</a>
 
-          <a href="#">
-            <img
-              class="avatar-large"
-              :src="userById(postById(postId).userId).avatar"
-              alt=""
-            />
-          </a>
-
-          <p class="desktop-only text-small">107 posts</p>
-        </div>
-
-        <div class="post-content">
-          <div>
-            <p>
-              {{ postById(postId).text }}
-            </p>
-          </div>
-        </div>
-
-        <div class="post-date text-faded">
-          {{ postById(postId).publishedAt }}
-        </div>
-      </div>
-    </div>
+    <post-list :posts="threadPosts" />
   </div>
 </template>
 
 <script>
 import sourceData from '@/data.json';
+import PostList from '@/components/PostList.vue';
+
 export default {
+  name: 'ThreadShow',
+  components: {
+    PostList,
+  },
   props: {
     id: {
       type: String,
@@ -52,7 +29,6 @@ export default {
     return {
       threads: sourceData.threads,
       posts: sourceData.posts,
-      users: sourceData.users,
     };
   },
 
@@ -61,14 +37,8 @@ export default {
       return this.threads.find((thread) => thread.id === this.id);
       // also available under this.$route.params.id
     },
-  },
-
-  methods: {
-    postById(postId) {
-      return this.posts.find((post) => post.id === postId);
-    },
-    userById(userId) {
-      return this.users.find((user) => user.id === userId);
+    threadPosts() {
+      return this.posts.filter((post) => post.threadId === this.id);
     },
   },
 };
