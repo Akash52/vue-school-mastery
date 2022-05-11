f0967b26e7218c42e4a117b368b4533963f7d5e6
Akash52 <ac8572611@gmail.com>
2022-05-11 16:25:50 +0530

    Fetching the Authenticated User From Vuex

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -18,11 +18,11 @@
           <a href="#">
             <img
               class="avatar-small"
-              src="https://pbs.twimg.com/profile_images/1188775562657091594/5mgkg44t_400x400.jpg"
-              alt=""
+              :src="authUser.avatar"
+              :alt="`${authUser.name} profile picture`"
             />
             <span>
-              Alex Kyriakidis
+              {{ authUser.name }}
               <img class="icon-profile" src="../assets/svg/arrow-profile.svg" alt="" />
             </span>
           </a>
@@ -67,7 +67,12 @@
 </template>
 
 <script>
-export default {};
+import { mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapGetters(["authUser"]),
+  },
+};
 </script>
 
 <style></style>
