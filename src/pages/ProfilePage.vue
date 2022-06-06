78c6d221cac962d54141fa66076b89d1ef1c431b
Akash52 <ac8572611@gmail.com>
2022-06-06 10:44:53 +0530

    Extracting User Posts and Threads to the Store

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -2,36 +2,7 @@
   <div class="container">
     <div class="flex-grid">
       <div class="col-3 push-top">
-        <div class="profile-card">
-          <p class="text-center">
-            <img
-              :src="user.avatar"
-              :alt="`${user.name} profile picture`"
-              class="avatar-xlarge"
-            />
-          </p>
-
-          <h1 class="title">{{ user.username }}</h1>
-
-          <p class="text-lead">{{ user.name }}</p>
-
-          <p class="text-justify">{{ user.bio || "No bio specified." }}</p>
-
-          <span class="online">{{ user.username }} is online</span>
-
-          <div class="stats">
-            <span>{{ userPostsCount }} posts</span>
-            <span>{{ userThreadsCount }} threads</span>
-          </div>
-
-          <hr />
-
-          <p v-if="user.website" class="text-large text-center">
-            <i class="fa fa-globe"></i>
-            <a :href="user.website">{{ user.website }}</a>
-          </p>
-        </div>
-
+        <UserProfileCard :user="user" />
         <p class="text-xsmall text-faded text-center">
           Member since june 2003, last visited 4 hours ago
         </p>
@@ -48,7 +19,7 @@
           <a href="#">See only started threads?</a>
         </div>
         <hr />
-        <PostListVue :posts="userPosts" />
+        <PostListVue :posts="user.posts" />
       </div>
     </div>
   </div>
@@ -57,28 +28,18 @@
 <script>
 import PostListVue from "@/components/PostList.vue";
 import { mapGetters } from "vuex";
+import UserProfileCard from "@/components/UserProfileCard.vue";
 
 export default {
   name: "ProfilePage",
   components: {
     PostListVue,
+    UserProfileCard,
   },
   computed: {
     ...mapGetters({
       user: "authUser",
     }),
-    userPosts() {
-      return this.$store.state.posts.filter((post) => post.userId === this.user.id);
-    },
-    userPostsCount() {
-      return this.userPosts.length;
-    },
-    userThreads() {
-      return this.$store.state.threads.filter((thread) => thread.userId === this.user.id);
-    },
-    userThreadsCount() {
-      return this.userThreads.length;
-    },
   },
 };
 </script>
